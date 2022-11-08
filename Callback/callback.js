18e2e6c5ee7b6fba6b6ec01c7d06c418b352ce33
EduCabreraMendoza <edu.cabrera.mendoza@gmail.com>
2022-11-07 21:16:32 -0600

    Función basica del funcionamiento de los callback

---
+++ Callback/callback.js
@@ -0,0 +1,11 @@
+function suma(a, b){
+    return a+b;
+}
+
+function promedio(a, b, callback){
+    let out = callback(a, b);
+    return out/2;
+}
+
+let res = promedio(2, 7, suma);
+console.log(res);
